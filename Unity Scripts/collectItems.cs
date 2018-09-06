58b69713774e56b442f4bdabaf4f7e1fc1215d39
Justin Ma <justinma3@gmail.com>
2018-09-05 21:21:10 -0500

    Update and rename collect.cs to collectItems.cs

--- Unity Scripts/collectItems.cs
+++ Unity Scripts/collectItems.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class collect : MonoBehaviour {
+public class collectItems : MonoBehaviour {
 
 	public string tag;
 	public GameObject[] dots;
