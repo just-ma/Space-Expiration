92e13f499a425a69dc82c1384e9185f594c3ceb9
Justin Ma <justinma3@gmail.com>
2018-09-05 21:18:06 -0500

    Update and rename make.cs to spawnAsteroids.cs

--- Unity Scripts/spawnAsteroids.cs
+++ Unity Scripts/spawnAsteroids.cs
@@ -2,13 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class make : MonoBehaviour {
+public class spawnAsteroids : MonoBehaviour {
 
 	public float dist = 15;
 	public GameObject[] blocks;
 	Vector3 avg;
 
-	// Use this for initialization
 	void Start () {
 		avg = transform.position;
 		StartCoroutine (build ());
@@ -45,13 +44,13 @@ public class make : MonoBehaviour {
 		rando = rando.normalized * dist;
 
 		int choice = 0;
-		int twerk = 0;
+		int torque = 0;
 		GameObject thisOne;
 		Rigidbody2D rb;
 
 		while (true){
 			choice = Random.Range (0, 2);
-			twerk = Random.Range (-2 + choice, 2 - choice) * 80;
+			torque = Random.Range (-2 + choice, 2 - choice) * 80;
 
 			yield return new WaitForSeconds(0.5f);
 			thisOne = Instantiate (blocks[choice], avg + rando, Quaternion.identity);
@@ -62,7 +61,7 @@ public class make : MonoBehaviour {
 
 			rb = thisOne.GetComponent<Rigidbody2D> ();
 			rb.AddForce ((rando) * 80f);
-			rb.AddTorque (twerk);
+			rb.AddTorque (torque);
 		}
 	}
 }
